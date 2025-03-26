f350005848c5946faf1dfbf47f1ec1965af3a2f4
Atul Shukla <atul.shukla@slashrtc.com>
2025-03-26 10:59:30 +0530

    Changes in api

--- routes/router.js
+++ routes/router.js
@@ -6,5 +6,7 @@ const authenticateToken = require('../helper/auth')
 router.get('/users',  controller.getAllClients);
 router.put('/update', controller.updateClient);
 router.post('/find', controller.getInvoice);
+router.post("/create", controller.createClient);
+router.delete("/delete/:businessId", controller.deleteClient);
 
 module.exports = router;
