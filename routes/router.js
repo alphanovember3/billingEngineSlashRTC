1cf045f0e703f86f14dc6ec968915d0dc03e24d4
alphanovember3 <ayushnalawade2233@gmail.com>
2025-03-27 11:01:54 +0530

    changes

--- routes/router.js
+++ routes/router.js
@@ -4,7 +4,7 @@ const controller = require('../controller/billingController');
 const authenticateToken = require('../helper/auth')
 
 router.get('/users',  controller.getAllClients);
-router.put('/update', controller.updateClient);
+router.patch('/update', controller.updateClient);
 router.post('/find', controller.getInvoice);
 router.post("/create", controller.createClient);
 router.delete("/delete", controller.deleteClient);
